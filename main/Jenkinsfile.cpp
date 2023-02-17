a1448ba42a070ffe1a7a08ee66a048b8c772d3b2
dhathriig <125532021+dhathriig@users.noreply.github.com>
2023-02-17 14:23:52 +0530

    Create Jenkinsfile.cpp

---
+++ main/Jenkinsfile.cpp
@@ -0,0 +1,32 @@
+ pipeline{
+    agent any
+    stages{
+      stage('Build'){
+        steps{
+          sh 'g++ -c PES1UG20CS141.cpp'
+          sh 'g++ -o PES1UG20CS141 PES1UG20CS141.cpp'
+          echo 'build stage successfull'
+        }
+      }
+      stage('Test'){
+        steps{
+          sh './PES1UG20CS141'
+          echo 'Test stage executed successfully'
+        }
+      }
+      stage('Deploy'){
+        steps{
+          echo 'Deployed successfully'
+        }
+      }
+    }
+  post{
+    always{
+      script{
+        if(currentBuild.result=='FAILURE'){
+          echo 'Pipeline Failed'
+        }
+      }
+    }
+  }
+}
